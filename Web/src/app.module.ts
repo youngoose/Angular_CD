881c54ed12debb0fc6a2bd8bf129c93807a6399c
Younghoon Jo (Eddy) <edwardevjo@gmail.com>
2017-12-10 13:22:24 -0500

    initial commit

--- Web/src/app.module.ts
+++ Web/src/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatTabsModule } from '@angular/material/tabs'
 
 import { AppContainer } from './Containers/App/app.component'
 import { SampleComponent } from './Components/Sample/sample.component'
+import { TabsComponent } from './components/tabs/tabs.component'
 
 @NgModule({
   declarations: [
     AppContainer,
-    SampleComponent
+    SampleComponent,
+    TabsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppContainer]
